6380aec034f35e14a7542d92db520c859cc78409
Mouloud Wassim <75916048+wass-sleepdart@users.noreply.github.com>
2021-11-26 23:12:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ for filename in os.listdir(f'./Cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'Cogs.{filename[:-3]}')
 
-bot.run('ODk5NzAyNjYwMzE0MzA4NjE4.YW2nZQ.PqbQpU2v9SBfQjKemXt1ATn05R4')
+bot.run('')
